d74835be5366eda6dfe923c2fbe80c09db11f715
Filipe Bacof <filipebacof@gmail.com>
2022-10-13 10:24:36 -0300

    componente finalizado

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -6,6 +6,15 @@ export default {
     component: Text,
     args: {
         children: 'Lorem Ipsum.',
+        size: 'md',
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio',
+            }
+        },
     },
 } as Meta<TextProps>
 
@@ -31,5 +40,12 @@ export const CustomComponent: StoryObj<TextProps> = {
         children: (
             <p>Testando</p>
         )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            }
+        }
     }
 }
